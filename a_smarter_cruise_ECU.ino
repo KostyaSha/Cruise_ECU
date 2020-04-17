c6f93340bcee4895ad4c25ddb9e6eed025060b0f
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-04-17 14:49:58 +0200

    Update a_smarter_cruise_ECU.ino

--- a_smarter_cruise_ECU.ino
+++ a_smarter_cruise_ECU.ino
@@ -27,7 +27,7 @@ float LEAD_REL_SPEED = 0;
 float LEAD_LONG_DIST_RAW = 0;
 float LEAD_REL_SPEED_RAW = 0;
 boolean BRAKE_PRESSED = true;
-
+boolean GAS_RELEASED = false;
 
 //______________FOR SMOOTHING SPD
 const int numReadings = 160;
@@ -148,7 +148,7 @@ if (buttonstate1 != lastbuttonstate1)
     
 if (pedalstate == LOW)
    {
-    Serial.println("Pedal is pressed");
+  //  Serial.println("Pedal is pressed");
    }
 
 lastbuttonstate1 = buttonstate1;
@@ -159,115 +159,115 @@ lastbuttonstate4 = buttonstate4;
 //______________SENDING_CAN_MESSAGES
 
   //0x1d2 msg PCM_CRUISE
-  uint8_t dat[8];
-  dat[0] = (OP_ON << 5) & 0x20;
-  dat[1] = 0x0;
-  dat[2] = 0x0;
-  dat[3] = 0x0;
-  dat[4] = 0x0;
-  dat[5] = 0x0;
-  dat[6] = (OP_ON << 7) & 0x80;
-  dat[7] = can_cksum(dat, 7, 0x1d2);
+  uint8_t dat_1d2[8];
+  dat_1d2[0] = (OP_ON << 5) & 0x20 | (GAS_RELEASED << 4) & 0x10;
+  dat_1d2[1] = 0x0;
+  dat_1d2[2] = 0x0;
+  dat_1d2[3] = 0x0;
+  dat_1d2[4] = 0x0;
+  dat_1d2[5] = 0x0;
+  dat_1d2[6] = (OP_ON << 7) & 0x80;
+  dat_1d2[7] = can_cksum(dat_1d2, 7, 0x1d2);
   CAN.beginPacket(0x1d2);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat[ii]);
+    CAN.write(dat_1d2[ii]);
   }
   CAN.endPacket();
 
   //0x1d3 msg PCM_CRUISE_2
-  uint8_t dat2[8];
-  dat2[0] = 0x0;
-  dat2[1] = (OP_ON << 7) & 0x80 | 0x28;
-  dat2[2] = set_speed;
-  dat2[3] = 0x0;
-  dat2[4] = 0x0;
-  dat2[5] = 0x0;
-  dat2[6] = 0x0;
-  dat2[7] = can_cksum(dat2, 7, 0x1d3);
+  uint8_t dat_1d3[8];
+  dat_1d3[0] = 0x0;
+  dat_1d3[1] = (OP_ON << 7) & 0x80 | 0x28;
+  dat_1d3[2] = set_speed;
+  dat_1d3[3] = 0x0;
+  dat_1d3[4] = 0x0;
+  dat_1d3[5] = 0x0;
+  dat_1d3[6] = 0x0;
+  dat_1d3[7] = can_cksum(dat_1d3, 7, 0x1d3);
   CAN.beginPacket(0x1d3);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat2[ii]);
+    CAN.write(dat_1d3[ii]);
   }
   CAN.endPacket();
 
   //0xaa msg defaults 1a 6f WHEEL_SPEEDS
-  uint8_t dat3[8];
+  uint8_t dat_aa[8];
   uint16_t wheelspeed = 0x1a6f + (average * 100);
-  dat3[0] = (wheelspeed >> 8) & 0xFF;
-  dat3[1] = (wheelspeed >> 0) & 0xFF;
-  dat3[2] = (wheelspeed >> 8) & 0xFF;
-  dat3[3] = (wheelspeed >> 0) & 0xFF;
-  dat3[4] = (wheelspeed >> 8) & 0xFF;
-  dat3[5] = (wheelspeed >> 0) & 0xFF;
-  dat3[6] = (wheelspeed >> 8) & 0xFF;
-  dat3[7] = (wheelspeed >> 0) & 0xFF;
+  dat_aa[0] = (wheelspeed >> 8) & 0xFF;
+  dat_aa[1] = (wheelspeed >> 0) & 0xFF;
+  dat_aa[2] = (wheelspeed >> 8) & 0xFF;
+  dat_aa[3] = (wheelspeed >> 0) & 0xFF;
+  dat_aa[4] = (wheelspeed >> 8) & 0xFF;
+  dat_aa[5] = (wheelspeed >> 0) & 0xFF;
+  dat_aa[6] = (wheelspeed >> 8) & 0xFF;
+  dat_aa[7] = (wheelspeed >> 0) & 0xFF;
   CAN.beginPacket(0xaa);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat3[ii]);
+    CAN.write(dat_aa[ii]);
   }
   CAN.endPacket();
 
   //0x3b7 msg ESP_CONTROL
-  uint8_t dat5[8];
-  dat5[0] = 0x0;
-  dat5[1] = 0x0;
-  dat5[2] = 0x0;
-  dat5[3] = 0x0;
-  dat5[4] = 0x0;
-  dat5[5] = 0x0;
-  dat5[6] = 0x0;
-  dat5[7] = 0x08;
+  uint8_t dat_3b7[8];
+  dat_3b7[0] = 0x0;
+  dat_3b7[1] = 0x0;
+  dat_3b7[2] = 0x0;
+  dat_3b7[3] = 0x0;
+  dat_3b7[4] = 0x0;
+  dat_3b7[5] = 0x0;
+  dat_3b7[6] = 0x0;
+  dat_3b7[7] = 0x08;
   CAN.beginPacket(0x3b7);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat5[ii]);
+    CAN.write(dat_3b7[ii]);
   }
   CAN.endPacket();
 
   //0x620 msg STEATS_DOORS
-  uint8_t dat6[8];
-  dat6[0] = 0x10;
-  dat6[1] = 0x0;
-  dat6[2] = 0x0;
-  dat6[3] = 0x1d;
-  dat6[4] = 0xb0;
-  dat6[5] = 0x40;
-  dat6[6] = 0x0;
-  dat6[7] = 0x0;
+  uint8_t dat_620[8];
+  dat_620[0] = 0x10;
+  dat_620[1] = 0x0;
+  dat_620[2] = 0x0;
+  dat_620[3] = 0x1d;
+  dat_620[4] = 0xb0;
+  dat_620[5] = 0x40;
+  dat_620[6] = 0x0;
+  dat_620[7] = 0x0;
   CAN.beginPacket(0x620);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat6[ii]);
+    CAN.write(dat_620[ii]);
   }
   CAN.endPacket();
 
   // 0x3bc msg GEAR_PACKET
-  uint8_t dat7[8];
-  dat7[0] = 0x0;
-  dat7[1] = 0x0;
-  dat7[2] = 0x0;
-  dat7[3] = 0x0;
-  dat7[4] = 0x0;
-  dat7[5] = 0x80;
-  dat7[6] = 0x0;
-  dat7[7] = 0x0;
+  uint8_t dat_3bc[8];
+  dat_3bc[0] = 0x0;
+  dat_3bc[1] = 0x0;
+  dat_3bc[2] = 0x0;
+  dat_3bc[3] = 0x0;
+  dat_3bc[4] = 0x0;
+  dat_3bc[5] = 0x80;
+  dat_3bc[6] = 0x0;
+  dat_3bc[7] = 0x0;
   CAN.beginPacket(0x3bc);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat7[ii]);
+    CAN.write(dat_3bc[ii]);
   }
   CAN.endPacket();
 
   //0x614 msg steering_levers
-  uint8_t dat614[8];
-  dat614[0] = 0x29;
-  dat614[1] = 0x0;
-  dat614[2] = 0x01;
-  dat614[3] = (blinker_left << 5) & 0x20 |(blinker_right << 4) & 0x10;
-  dat614[4] = 0x0;
-  dat614[5] = 0x0;
-  dat614[6] = 0x76;
-  dat614[7] = can_cksum(dat614, 7, 0x614);
+  uint8_t dat_614[8];
+  dat_614[0] = 0x29;
+  dat_614[1] = 0x0;
+  dat_614[2] = 0x01;
+  dat_614[3] = (blinker_left << 5) & 0x20 |(blinker_right << 4) & 0x10;
+  dat_614[4] = 0x0;
+  dat_614[5] = 0x0;
+  dat_614[6] = 0x76;
+  dat_614[7] = can_cksum(dat_614, 7, 0x614);
   CAN.beginPacket(0x614);
   for (int ii = 0; ii < 8; ii++) {
-    CAN.write(dat614[ii]);
+    CAN.write(dat_614[ii]);
   }
   CAN.endPacket();
 
@@ -277,12 +277,12 @@ lastbuttonstate4 = buttonstate4;
   //128x2e6 msg LEAD_INFO
   if (CAN.packetId() == 0x2e6)
       {
-      uint8_t dat_LEAD_INFO[8];
+      uint8_t dat_2e6[8];
       for (int ii = 0; ii <= 7; ii++) {
-        dat_LEAD_INFO[ii]  = (char) CAN.read();
+        dat_2e6[ii]  = (char) CAN.read();
         }
-        LEAD_LONG_DIST_RAW = (dat_LEAD_INFO[0] << 8 | dat_LEAD_INFO[1] << 3); 
-        LEAD_REL_SPEED_RAW = (dat_LEAD_INFO[2] << 8 | dat_LEAD_INFO[3] << 4);
+        LEAD_LONG_DIST_RAW = (dat_2e6[0] << 8 | dat_2e6[1] << 3); 
+        LEAD_REL_SPEED_RAW = (dat_2e6[2] << 8 | dat_2e6[3] << 4);
         }
   
   //0x224 msg BRAKE_MODULE
@@ -294,8 +294,23 @@ lastbuttonstate4 = buttonstate4;
         }
         BRAKE_PRESSED = (dat_224[0] << 5);
         }
-Serial.println(BRAKE_PRESSED);
   
+Serial.print("BRAKE_PRESSED = ");
+Serial.print(BRAKE_PRESSED);
+  
+    //0x2c1 msg GAS_PEDAL
+    if (CAN.packetId() == 0x2c1)
+      {
+      uint8_t dat_2c1[8];
+      for (int ii = 0; ii <= 7; ii++) {
+        dat_2c1[ii]  = (char) CAN.read();
+        }
+        GAS_RELEASED = (dat_2c1[0] << 3);
+        }
+Serial.print("GAS_RELEASED = ");
+Serial.print(GAS_RELEASED);
+Serial.println("");
+    
 }
 
 void rpm() {
